bcd9c37a09a0f739ddd759f0b04d2f95b025b320
Qandeel90 <qandeeltariq90@gmail.com>
2022-02-11 21:46:41 +0500

    change filters

--- routes/products.js
+++ routes/products.js
@@ -30,59 +30,6 @@ const storage = multer.diskStorage({
 
 const uploadOptions = multer({ storage: storage });
 
-router.get(`/`, async (req, res) => {
-  let filter = {};
-  if (req.query.categories) {
-    filter = { category: req.query.categories.split(",") };
-  }
-
-  const productList = await Product.find(filter).populate("category");
-
-  if (!productList) {
-    res.status(500).json({ success: false });
-  }
-  res.send(productList);
-});
-
-router.get(`/:id`, async (req, res) => {
-  const product = await Product.findById(req.params.id).populate("category");
-
-  if (!product) {
-    res.status(500).json({ success: false });
-  }
-  res.send(product);
-});
-
-router.post(`/`, uploadOptions.single("image"), async (req, res) => {
-  const category = await Category.findById(req.body.category);
-  if (!category) return res.status(400).send("Invalid Category");
-
-  const file = req.file;
-  if (!file) return res.status(400).send("No image in the request");
-
-  const fileName = file.filename;
-  const basePath = `${req.protocol}://${req.get("host")}/public/uploads/`;
-  let product = new Product({
-    name: req.body.name,
-    description: req.body.description,
-    richDescription: req.body.richDescription,
-    image: `${basePath}${fileName}`, // "http://localhost:3000/public/upload/image-2323232"
-    brand: req.body.brand,
-    price: req.body.price,
-    category: req.body.category,
-    countInStock: req.body.countInStock,
-    rating: req.body.rating,
-    numReviews: req.body.numReviews,
-    isFeatured: req.body.isFeatured,
-  });
-
-  product = await product.save();
-
-  if (!product) return res.status(500).send("The product cannot be created");
-
-  res.send(product);
-});
-
 router.put("/:id", uploadOptions.single("image"), async (req, res) => {
   if (!mongoose.isValidObjectId(req.params.id)) {
     return res.status(400).send("Invalid Product Id");
@@ -156,16 +103,6 @@ router.get(`/get/count`, async (req, res) => {
   res.send({
     productCount: productCount,
   });
-});
-
-router.get(`/get/featured/:count`, async (req, res) => {
-  const count = req.params.count ? req.params.count : 0;
-  const products = await Product.find({ isFeatured: true }).limit(+count);
-
-  if (!products) {
-    res.status(500).json({ success: false });
-  }
-  res.send(products);
 });
 
 router.put(
