51cf6dd675921cbc8a5d395d22c11f19de443e58
Qandeel90 <qandeeltariq90@gmail.com>
2022-02-11 16:41:01 +0500

    products schema change

--- models/product.js
+++ models/product.js
@@ -1,69 +1,55 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  richDescription: {
+    type: String,
+    default: "",
+  },
+  image_url: {
+    type: String,
+    default: "",
+  },
+  images: [
+    {
+      type: String,
     },
-    description: {
-        type: String,
-        required: true
-    },
-    richDescription: {
-        type: String,
-        default: ''
-    },
-    image: {
-        type: String,
-        default: ''
-    },
-    images: [{
-        type: String
-    }],
-    brand: {
-        type: String,
-        default: ''
-    },
-    price : {
-        type: Number,
-        default:0
-    },
-    category: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category',
-        required:true
-    },
-    countInStock: {
-        type: Number,
-        required: true,
-        min: 0,
-        max: 255
-    },
-    rating: {
-        type: Number,
-        default: 0,
-    },
-    numReviews: {
-        type: Number,
-        default: 0,
-    },
-    isFeatured: {
-        type: Boolean,
-        default: false,
-    },
-    dateCreated: {
-        type: Date,
-        default: Date.now,
-    },
-})
+  ],
 
-productSchema.virtual('id').get(function () {
-    return this._id.toHexString();
+  price: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-productSchema.set('toJSON', {
-    virtuals: true,
+productSchema.virtual("id").get(function () {
+  return this._id.toHexString();
 });
 
+productSchema.set("toJSON", {
+  virtuals: true,
+});
 
-exports.Product = mongoose.model('Product', productSchema);
+exports.Product = mongoose.model("Product", productSchema);
