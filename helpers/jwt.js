0145de758460fb357cd73a51b9d4687fe2ed8bfd
Qandeel90 <qandeeltariq90@gmail.com>
2022-03-10 19:59:56 +0500

    add route

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -10,9 +10,9 @@ function authJwt() {
   }).unless({
     path: [
       { url: /\/public\/uploads(.*)/, methods: ["GET", "OPTIONS"] },
-
       { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
-
+      { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/orders(.*)/, methods: ["GET", "OPTIONS", "POST"] },
       `${api}/users/login`,
       `${api}/users/register`,
     ],
