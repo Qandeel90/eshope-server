1da789a325de4e549c312aef835d95369a2a90a2
Qandeel90 <qandeeltariq90@gmail.com>
2022-03-10 20:07:49 +0500

    add category route

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ app.use(authJwt());
 app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
 app.use(errorHandler);
 //Routes
-
+const categoriesRoutes = require("./routes/categories");
 const productsRoutes = require("./routes/products");
 const usersRoutes = require("./routes/users");
 const ordersRoutes = require("./routes/orders");
 
 const api = process.env.API_URL;
-
+app.use(`${api}/categories`, categoriesRoutes);
 app.use(`${api}/products`, productsRoutes);
 app.use(`${api}/users`, usersRoutes);
 app.use(`${api}/orders`, ordersRoutes);
